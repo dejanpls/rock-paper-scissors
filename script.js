3ad62ce2114f648a87d6ed781093c2ed5c13d11b
dejanpls <68787353+dejanpls@users.noreply.github.com>
2023-04-17 18:43:07 +0200

    update getComputerChoice function

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-console.log("Hello World!");
+let randomNumber = Math.floor(Math.random() * 9) + 1; // random numbers from 1 to 9
+
+
+function getComputerChoice() {
+
+    if (randomNumber <= 3) return "Rock";
+    else if (randomNumber <= 6) return "Paper";
+    else return "Scissors";
+  }
+
+console.log(getComputerChoice());
