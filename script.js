87bf64fafee99122a6c813405a58ecbbea819ee8
dejanpls <68787353+dejanpls@users.noreply.github.com>
2023-04-17 18:53:39 +0200

    create getRAndomNumber and getUserChoice function

--- script.js
+++ script.js
@@ -1,11 +1,18 @@
-let randomNumber = Math.floor(Math.random() * 9) + 1; // random numbers from 1 to 9
+"use strict";
 
+function getRandomNumber() {
+  // random number from 1 to 9
+  return Math.floor(Math.random() * 9) + 1;
+}
 
 function getComputerChoice() {
-
-    if (randomNumber <= 3) return "Rock";
-    else if (randomNumber <= 6) return "Paper";
+    if (getRandomNumber() <= 3) return "Rock";
+    else if (getRandomNumber() <= 6) return "Paper";
     else return "Scissors";
   }
 
-console.log(getComputerChoice());
+
+function getUserChoice(choice) {
+  choice = prompt("Rock, Paper or Scissors?").toLowerCase();
+  return choice;
+}
