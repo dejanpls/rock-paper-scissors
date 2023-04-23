21f6f0f51fb76fba2be206419c62ef56c9610f20
dejanpls <68787353+dejanpls@users.noreply.github.com>
2023-04-23 21:39:00 +0200

    tidy up the code and create some additional functions to make the code more readable

--- script.js
+++ script.js
@@ -1,31 +1,90 @@
-"use strict";
+"use strict"
 
-function getRandomNumber() {
-  // random number from 1 to 9
-  return Math.floor(Math.random() * 9) + 1;
-}
+let playerScore = 0;
+let computerScore = 0;
+
+let roundOfGame = "";
+
+let winnerMsg = "";
+let loserMsg = "";
+let tieMsg = "";
+
+let getUserChoice = "";
+
+let finalScoreMsg = "";
+let displayScore = "";
+
+let gameActive = true;
 
 
-function getComputerChoice() {
-    if (getRandomNumber() <= 3) return "rock";
-    else if (getRandomNumber() <= 6) return "paper";
-    else return "scissors";
-  }
+//get random number from 1 to 3
+let getRandomNumber = () => Math.floor(Math.random() * 3 + 1);
 
 
-function playRound(playerSelection, computerSelection) {
-  let winner = "You win! Congratulations!";
-  let loser = "You lose, better luck next time!"
-  let tie = "It's a tie."
+let getComputerChoice = function() {
+	if (getRandomNumber() === 1) return "rock"
+	else if(getRandomNumber() === 2) return "paper"
+	else return "scissors"
+};
 
-  if (playerSelection === "paper" && computerSelection === "scissors") return loser;
-  else if (playerSelection === "rock" && computerSelection === "paper") return loser;
-  else if (playerSelection === "scissors" && computerSelection === "rock") return loser;
-  else if (playerSelection === computerSelection) return tie;
-  else return winner;
+//using if else statement returns a variable according to the winner of the round
+let playRound = function(playerSelection, computerSelection) {
+	if (playerSelection === computerSelection) {
+		return tieMsg = `The game is tie! ${playerSelection} equals ${computerSelection}.`;
+	} else if (
+		(playerSelection === "paper" && computerSelection === "rock") ||
+		(playerSelection === "rock" && computerSelection === "scissors") ||
+		(playerSelection === "scissors" && computerSelection === "paper")
+	) {
+		return winnerMsg = `It's a win for you! ${playerSelection} beats ${computerSelection}.`;
+	} else {
+		return loserMsg = `Computer wins! ${playerSelection} can't beat ${computerSelection}.`;
+	}
+};
 
-}
 
-const userChoice = prompt("Rock, Paper or Scissors?").toLowerCase();
+let playGame = function(userSelection) {
+	getUserChoice = prompt("Rock, Paper or Scissors?");
 
-console.log(playRound(userChoice, getComputerChoice()));
+	roundOfGame = playRound(getUserChoice, getComputerChoice());
+
+	console.log(roundOfGame);
+};
+
+
+let calcResult = function(roundResult) {
+	if (roundResult === winnerMsg) return playerScore += 1
+	else if (roundResult === loserMsg) return computerScore += 1
+};
+
+
+let endGame = () => {
+	if (playerScore === 5 || computerScore === 5) gameActive = false;
+};
+
+
+let getFinalScore = function() {
+		if (playerScore > computerScore) return finalScoreMsg = `You win! Congratulations!`
+		else return finalScoreMsg = `You lose. Better luck next time...`
+};
+
+
+let showResults = function() {
+		displayScore = `Final score: ${playerScore}:${computerScore}`;
+		console.log(getFinalScore(), displayScore);
+};
+
+
+let game = function() {
+
+	while (gameActive) {
+		playGame();
+		calcResult(roundOfGame);
+		endGame();
+	}
+
+	showResults();
+
+};
+
+game();
